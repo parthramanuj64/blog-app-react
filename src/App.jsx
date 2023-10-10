da10a419f24847ea006a4ae813a8cc8752654a94
parthramanuj64 <parthramanuj64@gmail.com>
2023-10-10 15:55:17 +0530

    Complete blog Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1>A Blog App With AppWrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentuser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Button from "./Button";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import InputField from "./InputField";
+import Login from "./Login";
+import Logo from "./Logo";
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Container from "./container/Container";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+import Select from "./Select";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  InputField,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+  AuthLayout,
+  Select
+};
